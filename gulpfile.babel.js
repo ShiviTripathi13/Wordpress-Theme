333851e16d3b30bd467518ddb034e08b1617a349
benten1013 <shivangi13091@gmail.com>
2023-09-27 19:50:27 +0530

    implementating of gulp-imagemin module(still fixing the bugs)

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,12 +2,56 @@
 
 import gulp from 'gulp';
 import yargs from 'yargs';
+import dartSass from 'sass'
+import gulpSass from 'gulp-sass'
+import cleanCSS from 'gulp-clean-css';
+import gulpIf from 'gulp-if';
+import sourcemaps from 'gulp-sourcemaps';
+//import imagemin from 'gulp-imagemin';
+const imagemin = require('gulp-imagemin');
+
+
+// let imagemin;
+// import('gulp-imagemin').then((gulpImagemin) => {
+//   imagemin = gulpImagemin;
+// });
+
+
+const sass = gulpSass(dartSass);
+const imagemin = await import('gulp-imagemin');
+
 
 const PRODUCTION = yargs.argv.prod;
 
-export const hello = (done) => {
-  console.log(PRODUCTION);
-  done();
+const paths = {
+  styles: {
+    src: ['src/assets/scss/bundle.scss', 'src/assets/scss/admin.scss'],
+    dest: 'dist/assests/css'
+  },
+  images: {
+    src: 'src/assets/images/**/*.{jpg,jpeg,png,svg,gif}',
+    dest: 'dist/assests/images'
+  }
 }
 
-export default hello;
+
+export const styles = () => {
+  return gulp.src(paths.styles.src)
+      .pipe(gulpIf(!PRODUCTION, sourcemaps.init()))
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulpIf(PRODUCTION, cleanCSS({compatibility: 'ie8'})))
+      .pipe(gulpIf(!PRODUCTION, sourcemaps.write()))
+      .pipe(gulp.dest(paths.styles.dest));
+}
+
+export const images = () => {
+  
+  return gulp.src(paths.images.src)
+    .pipe(gulpIf(PRODUCTION, imagemin()))
+    .pipe(gulp.dest(paths.images.dest));
+}
+
+export const watch = () => {
+  gulp.watch('src/assets/scss/**/*.scss', styles);
+}
+//export default styles;
